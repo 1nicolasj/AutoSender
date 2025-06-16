f3ccd229fac283e4f45bb6d70266620a33f26dc0
Nicolas <1nicolas.dasilvaa@gmail.com>
2025-06-16 10:01:17 -0300

    WIP

---
+++ AutoSender/Controllers/ContatoController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutoSender.Models;
+
+namespace AutoSender.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ContatoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contato
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ContatoModel.ToListAsync());
+        }
+
+        // GET: Contato/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contatoModel = await _context.ContatoModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contatoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contatoModel);
+        }
+
+        // GET: Contato/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contato/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,NumeroTelefone,Mensagem")] ContatoModel contatoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contatoModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contatoModel);
+        }
+
+        // GET: Contato/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contatoModel = await _context.ContatoModel.FindAsync(id);
+            if (contatoModel == null)
+            {
+                return NotFound();
+            }
+            return View(contatoModel);
+        }
+
+        // POST: Contato/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,NumeroTelefone,Mensagem")] ContatoModel contatoModel)
+        {
+            if (id != contatoModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contatoModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContatoModelExists(contatoModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contatoModel);
+        }
+
+        // GET: Contato/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contatoModel = await _context.ContatoModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (contatoModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(contatoModel);
+        }
+
+        // POST: Contato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contatoModel = await _context.ContatoModel.FindAsync(id);
+            if (contatoModel != null)
+            {
+                _context.ContatoModel.Remove(contatoModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContatoModelExists(int id)
+        {
+            return _context.ContatoModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ AutoSender/Models/ContatoModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoSender.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Phone]
+        public string NumeroTelefone { get; set; } 
+        public string Mensagem { get; set; } = string.Empty;
+    }
+}
